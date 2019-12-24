afe90a917fb82f88eafdf286a1b953f20d83d457
Daniel Tamas <hello@danieltamas.ro>
2019-12-24 23:26:43 +0200

    added extra fields to the Paypal request

--- src/Services/ExpressCheckout.php
+++ src/Services/ExpressCheckout.php
@@ -91,8 +91,8 @@ class ExpressCheckout
                 'L_PAYMENTREQUEST_0_DESC'.$num  => isset($item['desc']) ? $item['desc'] : null,
                 'L_PAYMENTREQUEST_0_QTY'.$num   => isset($item['qty']) ? $item['qty'] : 1,
                 'L_PAYMENTREQUEST_0_TAXAMT'.$num   => isset($item['tax']) ? $item['tax'] : 0,
-                'L_PAYMENTREQUEST_0_ITEMCATEGORY'.$num => $item['category'],
-                'L_PAYMENTREQUEST_0_ITEMURL'.$num => $item['url']
+                'L_PAYMENTREQUEST_0_ITEMCATEGORY'.$num => isset($item['category']) ? $item['category'] : 'DIGITAL_GOODS',
+                'L_PAYMENTREQUEST_0_ITEMURL'.$num => isset($item['url']) ? $item['url'] : null
             ];
         })->flatMap(function ($value) {
             return $value;
